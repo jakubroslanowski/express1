5f3d5e88eb0a3cc441ba14845a000454d8216023
Jakub Rosłanowski <jakub.roslanowski@gmail.com>
2021-02-05 13:49:53 +0100

    Use hbs to render

--- server.js
+++ server.js
@@ -1,41 +1,39 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
-app.use('/user', (req, res, next) => {
-  res.show('forbidden.html');
-});
-
-app.get('/404.jpg', (req, res) => {
-  res.sendFile(path.join(__dirname, '/404.jpg'));
-});
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 
-app.get('/user', (req, res) => {
-  res.show('about.html');
+app.get('/contact', (req, res) => {
+  res.render('contact');
 });
 
-app.get('/home', (req, res) => {
-  res.show('home.html');
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.status(404).send('404 not found...');
 });
 
 app.listen(8000, () => {
